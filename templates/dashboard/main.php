52aea10077b457a885ba685f1572594c4698e047
Casey Milne <casey.milne@eatbuildplay.com>
2020-10-15 09:13:30 -0500

    added dashboard section

--- templates/dashboard/main.php
+++ templates/dashboard/main.php
@@ -1,1 +1,41 @@
-MAIN DAHS....
+<?php
+
+$countPostTypes = wp_count_posts('acfe_post_type');
+
+// var_dump( $countPostTypes );
+
+?>
+
+<div class="acfg-dashboard">
+
+  <header>
+    <h1>ACF Engine</h1>
+  </header>
+
+  <div class="acfg-dashboard-row">
+
+    <div class="acfg-dashboard-item">
+
+      <h2>Post Types</h2>
+      <h3 class="acfg-dashboard-stat">
+        <?php print $countPostTypes->publish; ?>
+      </h3>
+      <h4>
+        <a href="edit.php?post_type=acfe_post_type">Manage Post Types</a>
+      </h4>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+<style>
+
+header {
+  background: #AAA;
+  color: #353535;
+}
+
+</style>
