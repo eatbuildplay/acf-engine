1bfbcef0e0419d6fe154d712654345c29f91cc69
Casey Milne <casey.milne@eatbuildplay.com>
2020-10-28 20:37:26 -0500

    add support for placeholders

--- src/BlockTypeBigHeadline.php
+++ src/BlockTypeBigHeadline.php
@@ -35,17 +35,38 @@ class BlockTypeBigHeadline extends BlockType {
 	protected function renderPreview( $block, $content, $editorPostId ) {
 
     $text = get_field('text');
+
+		// check for field placeholders
+		if( strpos('{{', $text) !== false ) {
+			$value = get_field( 'heading', $editorPostId );
+		} else {
+			$value = $text;
+		}
+
     print '<h2 style="font-size: 4.5em;">';
-    print $text;
+    print $value;
     print '</h2>';
 
 	}
 
 	protected function render( $block, $content, $editorPostId ) {
 
-    $text = get_field('text');
+		$value = get_field('text');
+
+		// check for field placeholders
+		if( strpos( $value, '{{' ) !== false ) {
+
+			preg_match_all('/{{(.*?)}}/', $value, $matches);
+			if( !empty( $matches[1] )) {
+				foreach( $matches[1] as $placeholder ) {
+					$placeholderValue = get_field( 'heading', $editorPostId );
+					$value = str_replace('{{'.$placeholder.'}}', $placeholderValue, $value);
+				}
+			}
+		}
+
     print '<h2 style="font-size: 4.5em;">';
-    print $text;
+    print $value;
     print '</h2>';
 
 	}
