2c4fcfc5010799516d8f4b656e6941149e1a801f
Casey Milne <casey.milne@eatbuildplay.com>
2020-11-09 15:30:05 -0500

    parse json to custom form object

--- src/FormManager.php
+++ src/FormManager.php
@@ -49,7 +49,7 @@ class FormManager {
 		$data->formFieldEl			= get_field('form_field_el', $postId);
 		$data->uploader					= get_field('uploader', $postId);
 		$data->honeypot					= get_field('honeypot', $postId);
-		$data->html_updated_message = get_field('html_updated_message', $postId);
+		$data->htmlUpdatedMessage = get_field('html_updated_message', $postId);
 		$data->htmlSubmitButton	= get_field('html_submit_button', $postId);
 		$data->htmlSubmitSpinner = get_field('html_submit_spinner', $postId);
 		$data->kses 						= get_field('kses', $postId);
@@ -84,36 +84,28 @@ class FormManager {
         $f->setKey( $data->key );
 				$f->setTitle( $data->title );
 				$f->setPostId( $data->postId );
-
-				/*
-
-				Continue adding remaining setter calls:
-
-				$data->newPost 					= get_field('new_post', $postId);
-				$data->fieldGroups 			= get_field('form_field_groups', $postId);
-				$data->fields 					= get_field('form_fields', $postId);
-				$data->postTitle 				= get_field('post_title', $postId);
-				$data->postContent 			= get_field('post_content', $postId);
-				$data->form 						= get_field('form', $postId);
-				$data->formAttributes 	= get_field('form_attributes', $postId);
-				$data->return 					= get_field('return', $postId);
-				$data->htmlBeforeFields = get_field('html_before_fields', $postId);
-				$data->htmlAfterFields 	= get_field('html_after_fields', $postId);
-				$data->submitValue 			= get_field('submit_value', $postId);
-				$data->updatedMessage 	= get_field('updated_message', $postId);
-				$data->labelPlacement		= get_field('label_placement', $postId);
-				$data->instructionPlacement = get_field('instruction_placement', $postId);
-				$data->formFieldEl			= get_field('form_field_el', $postId);
-				$data->uploader					= get_field('uploader', $postId);
-				$data->honeypot					= get_field('honeypot', $postId);
-				$data->html_updated_message = get_field('html_updated_message', $postId);
-				$data->htmlSubmitButton	= get_field('html_submit_button', $postId);
-				$data->htmlSubmitSpinner = get_field('html_submit_spinner', $postId);
-				$data->kses 						= get_field('kses', $postId);
-
-				*/
-
-        $c->register();
+				$f->setNewPost( $data->newPost );
+				$f->setFieldGroups( $data->fieldGroups );
+				$f->setFields( $data->fields );
+				$f->setPostTitle( $data->postTitle );
+				$f->setPostContent( $data->postContent );
+				$f->setForm( $data->form );
+				$f->setFormAttributes( $data->formAttributes );
+				$f->setReturn( $data->return );
+				$f->setHtmlBeforeFields( $data->htmlBeforeFields );
+				$f->setHtmlAfterFields( $data->htmlAfterFields );
+				$f->setSubmitValue( $data->submitValue );
+				$f->setUpdatedMessage( $data->updatedMessage );
+				$f->setLabelPlacement( $data->labelPlacement );
+				$f->setInstructionPlacement( $data->instructionPlacement );
+				$f->setFormFieldEl( $data->formFieldEl );
+				$f->setUploader( $data->uploader );
+				$f->setHoneypot( $data->honeypot );
+				$f->setHtmlUpdatedMessage( $data->htmlUpdatedMessage );
+				$f->setHtmlSubmitButton( $data->htmlSubmitButton );
+				$f->setHtmlSubmitSpinner( $data->htmlSubmitSpinner );
+				$f->setKses( $data->kses );
+        $f->register();
 
       }
 
