c6b1a704b583ed305cf5e6c3d5eb73165cea46c9
Casey Milne <casey.milne@eatbuildplay.com>
2020-11-13 11:25:26 -0500

    group field template

--- templates/fields/group/default.php
+++ templates/fields/group/default.php
@@ -2,10 +2,29 @@
 
 // https://www.advancedcustomfields.com/resources/group/
 
-/*
+//var_dump( $field );
 
-Group has to be handled similarly to repeater. 
+while ( have_rows( $field['key'], $postId )) :
 
-*/
+  the_row();
+  $content = '';
+
+  foreach( $field['sub_fields'] as $index => $subfield ) :
+
+    $subfieldValue = get_sub_field( $subfield['key'] );
+    $tl = new \AcfEngine\Core\TemplateLoader();
+    $tl->path = 'templates/fields/' . $subfield['type'] . '/';
+    $tl->name = 'default';
+    $tl->data = [
+      'field'  => $subfield,
+      'value'  => $subfieldValue,
+      'postId' => $postId
+    ];
+    $content .= $tl->get();
+
+  endforeach;
+endwhile;
+
+print $content;
 
 ?>
